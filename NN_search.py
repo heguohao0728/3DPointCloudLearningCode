b6bbb92f18173816b639a83efa2f44a8e973e880
heguohao0728 <1025264526@qq.com>
2020-12-16 09:24:05 +0800

    Create NN_search.py

---
+++ NN_search.py
@@ -0,0 +1,42 @@
+import numpy as np
+from tree_node import Node
+from NN_container import KNNResultSet, RadiusNNResultSet
+import math
+
+
+def knn_search(root : Node, result_set : KNNResultSet, key):
+    if root is None:
+        return False
+    result_set.add_point(math.fabs(root.key-key), root.value)
+    if result_set.worst_distance() == 0:
+        return True
+    if key <= root.key:
+        if knn_search(root.left, result_set, key):
+            return True
+        elif math.fabs(root.key - key) < result_set.worst_distance():
+            return knn_search(root.right, result_set, key)
+        return False
+    else:
+        if knn_search(root.right, result_set, key):
+            return True
+        elif math.fabs(root.key - key) < result_set.worst_distance():
+            return knn_search(root.left, result_set, key)
+        return False
+
+
+def radius_search(root : Node, result_set : RadiusNNResultSet, key):
+    if root is None:
+        return False
+    result_set.add_point(math.fabs(root.key-key), root.value)
+    if key <= root.key:
+        if radius_search(root.left, result_set, key):
+            return True
+        elif math.fabs(root.key - key) < result_set.worst_radius():
+            return radius_search(root.right, result_set, key)
+        return False
+    else:
+        if radius_search(root.right, result_set, key):
+            return True
+        elif math.fabs(root.key - key) < result_set.worst_radius():
+            return radius_search(root.left, result_set, key)
+        return False
