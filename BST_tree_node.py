18ca0341c78f0bf672058d0bebf4a2f606c36f54
heguohao0728 <1025264526@qq.com>
2020-12-16 10:51:50 +0800

    Create BST_tree_node.py

---
+++ BST_tree_node.py
@@ -0,0 +1,12 @@
+"""BST Node class"""
+
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.value = value
+
+
+'''BST Node class'''
